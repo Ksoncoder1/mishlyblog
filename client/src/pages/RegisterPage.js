fbb5c2191865b47761665da346396bab6ed86649
Ksoncoder1 <113189184+Ksoncoder1@users.noreply.github.com>
2023-07-27 08:58:02 +0300

    Update RegisterPage.js

--- client/src/pages/RegisterPage.js
+++ client/src/pages/RegisterPage.js
@@ -6,7 +6,7 @@ export default function RegisterPage (){
 
     async function register(ev){
         ev.preventDefault();
-        const response = await fetch('http://localhost:4000/register', {
+        const response = await fetch('mishlyblog-api.vercel.app/register', {
         //const response = await fetch(url, {
             method: 'POST',
             body: JSON.stringify({userName, password}),
