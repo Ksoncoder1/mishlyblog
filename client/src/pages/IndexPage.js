a227da483e4159d60bb7454cf6c08c62e608ce08
Ksoncoder1 <113189184+Ksoncoder1@users.noreply.github.com>
2023-07-27 08:52:33 +0300

    Update IndexPage.js

--- client/src/pages/IndexPage.js
+++ client/src/pages/IndexPage.js
@@ -5,7 +5,7 @@ export default function IndexPage (){
   const [posts, setPosts] = useState([]);
   const url = `${process.env.REACT_APP_API_URL}/post`;
   useEffect(() =>{
-    fetch('http://localhost:4000/post').then(response => {
+    fetch('mishlyblog-api.vercel.app/post').then(response => {
     //fetch(url).then(response => {
       response.json().then(posts => {
         setPosts(posts);
